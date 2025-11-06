a9d9cae6b1b5c5ee3d3350ece587cee8a99eeed6
Waheed Akinrele <waheedakinrele@yahoo.com>
2025-11-06 23:37:15 +0330

    Trigger redeploy with new API URL

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,8 +1,2 @@
 // src/Config/api.js
-
-    // src/Config/api.js
-export const API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://ecommerce-kf6inh0gw-wahedd12s-projects.vercel.app" // no /api at the end
-    : "http://localhost:5000";
-
+export const API_URL = import.meta.env.VITE_API_URL;
